c7374e0a79239a6ba69e4901cee1542753db5edb
Ahuna2 <44437550+Ahuna2@users.noreply.github.com>
2019-05-13 00:51:53 +0300

    Update main.java
    GUI compatibility

--- main.java
+++ main.java
@@ -1,7 +1,16 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
 import java.io.IOException;
 
-public class main {
+public class main extends Application {
     public static void main(String[] args) throws IOException {
-        MainCycle.runtime();
+        //MainCycle.runtime();
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Reader reader = new Reader();
     }
 }
