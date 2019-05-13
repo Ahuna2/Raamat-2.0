2fe964817990ba1baa651e3c9c564e143a037236
Ahuna2 <44437550+Ahuna2@users.noreply.github.com>
2019-05-13 11:22:40 +0300

    Update Reader.java

--- Reader.java
+++ Reader.java
@@ -1,28 +1,28 @@
-import javafx.scene.Scene;
-
 import java.io.File;
 import java.io.IOException;
 
-public class Reader {
-
-    private File projectsFolder = new File(System.getProperty("user.dir")
-            + System.getProperty("file.separator") + "Saved Projects");
-    private Scene window;
+class Reader {
 
 
-    public Reader() throws IOException {
-        if (!this.projectsFolder.exists()) {
-            this.projectsFolder.mkdir();
+    Reader() throws IOException {
+        //OTSUSTAB, KAS LIIGUB PROJEKTIVALIKU JUURDE VÕI ANNAB NOPROJECTSALERT ALERDI
+
+        //Kui "Saved Projects" folder ei eksisteeri, anna alert ja sulgu.
+        File projectsFolder = new File(System.getProperty("user.dir")
+                + System.getProperty("file.separator") + "Saved Projects");
+
+        if (!projectsFolder.exists()) {
+            projectsFolder.mkdir();
             NoProjectsAlert.display();
         }
-        else if (this.projectsFolder.listFiles().length == 0) {
+        //Kui "Saved Projects" folder on tühi, anna alert ja sulgu.
+        else if (projectsFolder.listFiles().length == 0) {
             NoProjectsAlert.display();
         }
+        //Muidu liigu valiku juurde.
         else {
-            Raamat openProject = ChooseProjectPage.chooseProject(this.projectsFolder);
+            Raamat openProject = ChooseProjectPage.chooseProject(projectsFolder);
             HandleReader.handle(openProject, "0", 500, 500);
         }
     }
-
-
 }
